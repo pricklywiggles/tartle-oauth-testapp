28f147b0d10cd717469e23a84b5f48ef4a02297e
Gustavo Gallegos <goose@fractal.ly>
2025-03-24 10:41:33 -0700

    fix layout bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
   return config.client_id ? (
     <div className="rounded-lg border-1 border-gray-500 bg-custom-blue p-8">
       <div className="flex flex-col gap-6 text-gray-200">
-        <span className="content-center justify-center text-center font-bold whitespace-nowrap text-gray-200">
+        <span className="content-center justify-center text-center font-bold text-gray-200">
           To test the authorization flow, click the button below
         </span>
         <DataVaultConnectButton
